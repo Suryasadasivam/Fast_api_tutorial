323d7a006f539218254e03fc0bfc49881d7c4cb1
Suryasadasivam <suryasadasivamm@gmail.com>
2025-07-20 19:58:20 +0530

    add name

--- Tutorial_2.py
+++ Tutorial_2.py
@@ -55,9 +55,9 @@ BOOKS = [
 
 @app.get("/books",status_code=status.HTTP_200_OK)
 async def read_all_books():
-    return Books
+    return BOOKS
 
-#Path validation
+#Path data validation
 @app.get("/books/{book_id}",status_code=status.HTTP_200_OK)
 async def read_all_books(book_id:int=Path(gt=0)):
     for book in BOOKS:
@@ -65,4 +65,54 @@ async def read_all_books(book_id:int=Path(gt=0)):
             return book
     raise HTTPException(status_code=404,detail="Item not found")
 
+#query data validation
+@app.get("/books/rating",status_code=status.HTTP_200_OK)
+async def read_all_books_ratings(book_rating:int=Query(gt=0,lt=6)):
+    book_result=[]
+    for book in BOOKS:
+        if book.rating==book_rating:
+            book_result.append(book)
+    return book_result
 
+@app.get("/books/publish",status_code=status.HTTP_200_OK)
+async def read_all_books_publish(book_rating:int=Query(gt=1999,lt=2031)):
+    book_result_publish=[]
+    for book in BOOKS:
+        if book.rating==book_rating:
+            book_result_publish.append(book)
+    return book_result_publish
+
+#Data validation 
+@app.post("/create_books",status_code=status.HTTP_201_CREATED)
+async def create_book(book_request:BookRequest):
+    new_book=Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+
+def find_book_id(book:Book):
+    book.id=1 if len(BOOKS)==0 else BOOKS[-1].id+1
+    return book
+
+
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
+async def update_book(book: BookRequest):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
+
+
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            book_changed = True
+            break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
+    
+# Dhanush 
